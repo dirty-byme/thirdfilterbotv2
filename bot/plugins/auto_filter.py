831538f484860c28f88d4d8a0df9e566e4f56db6
dirty-byme <85059248+dirty-byme@users.noreply.github.com>
2021-06-11 11:52:19 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                text=f"Found {(len_results)} Request ചെയ്ത file കിട്ടിയില്ലെങ്കിൽ പിന്നിട് അഡ്മിൻ തരുന്നതായിരിക്കും Results For Your Request: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
